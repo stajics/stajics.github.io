31c36d67cec2705de202334588a02ab3861abe60
Srdjan Stajic <stajic.srdjan92@gmail.com>
2020-08-24 22:45:24 +0200

    Add files via upload

---
+++ component---src-pages-index-js-ea640f9b186b82a88625.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{RXBc:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),r=a.n(n),o=a("Bl7J"),c=a("Wbzz");t.default=function(e){var t=Object(c.c)("4224701694");return r.a.createElement(o.a,{pageTitle:"About",pageDescription:"Learn more about me"},r.a.createElement("div",{dangerouslySetInnerHTML:{__html:t.site.siteMetadata.about}}))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-ea640f9b186b82a88625.js.map
